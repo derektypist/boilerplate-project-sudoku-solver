03d760d6d1997a230f412c9a6429dcfa83ac273e
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-02-19 17:39:35 +0000

    Update 1_unit-tests.js

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -1,133 +1,81 @@
-/*
- *
- *
- *       FILL IN EACH UNIT TEST BELOW COMPLETELY
- *       -----[Keep the tests in the same order!]----
- *       (if additional are added, keep them at the very end!)
- */
-
 const chai = require('chai');
 const assert = chai.assert;
 
 const Solver = require('../controllers/sudoku-solver.js');
-let solver;
+let solver=new Solver();
+
 
 suite('UnitTests', () => {
-  suiteSetup(done => {
-    solver = new Solver();
-    done();
-  });
-
-  suite('Function validate()', () => {
-    test('Valid Characters, length of 81', (done) => {
-      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-
-      // done();
+  suite('Validate puzzle string', () => {
+    test('handles a valid puzzle string of 81 characters', () => {
+      let validStr = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+      let isValid = solver.validate(validStr).result;
+      assert.isTrue(isValid);
+      assert.equal(81,validStr.length);
     });
-
-    // Invalid characters or numbers are not accepted 
-    // as valid input for the puzzle grid
-    test('Invalid characters (anything other than "1-9" or "."") are not accepted', (done) => {
-      const input = '..X..5.1.85.4....2432.HI...1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      const error = "Invalid characters in puzzle";
-
-      // done();
+    test('handles a puzzle string with invalid characters (not 1-9 or .)',() => {
+      let invalidStr = '#.839.7.575.....964..1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1';
+      let isValid = solver.validate(invalidStr).result;
+      assert.isFalse(isValid);
     });
-
-    // Puzzles that are not 81 numbers/periods long show the message 
-    test('Shows an error for puzzles that are not 81 numbers long', done => {
-      const shortStr = '83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      const longStr = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6...';
-      const errorMsg = 'Expected puzzle to be 81 characters long';
-      
-      
-      // done();
+    test('handles a puzzle string that is not 81 characters in length',() => {
+      let invalidStr = '.........9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+      let isValid = solver.validate(invalidStr).result;
+      let invalidStrLen = invalidStr.length;
+      assert.isFalse(isValid);
+      assert.notEqual(81, invalidStrLen);
     });
   });
-  
-  suite('Function checkRowPlacement()', () => {
-    test('Valid placement for a row', done => {
-      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      const row = 0;
-      const col = 0;
-      const value = 3;
-      
-      // done();
-    });
-    
-    test('Invalid placement for a row', done => {
-      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      const row = 0;
-      const col = 0;
-      const value = 9;
 
-      // done();
-    });
-    
-  });
-
-  suite('Function checkColPlacement()', () => {
-    test('Valid placement for a column', done => {
-      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      const row = 0;
-      const col = 0;
-      const value = 3;
-      
-      // done();
-    });
-    
-    test('Invalid placement for a column', done => {
-      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      const row = 0;
-      const col = 0;
-      const value = 9;
-
-      // done();
-    });
-    
-  });
-
-  suite('Function checkRegionPlacement()', () => {
-    test('Valid placement for a region', done => {
-      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      const row = 4;
-      const col = 4;
-      const value = 3;
-      
-      // done();
-    });
-    
-    test('Invalid placement for a region', done => {
-      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      const row = 4;
-      const col = 4;
-      const value = 7;
-
-      // done();
-    });
-    
-  });
-
-  suite('Function solvePuzzle()', () => {
-    // Valid complete puzzles pass
-    test('Valid puzzles pass', done => {
-      const input = '769235418851496372432178956174569283395842761628713549283657194516924837947381625';
-
-      // done();
+  suite('Validates placement',() => {
+    let puzzleString = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+    test('handles valid row placement',() => {
+      let isValid = solver.checkRowPlacement(puzzleString,'A',1,'3');
+      assert.isTrue(isValid);
     });
 
-    // Invalid complete puzzles fail
-    test('Invalid puzzles fail', done => {
-      const input = '779235418851496372432178956174569283395842761628713549283657194516924837947381625';
-
-      // done();
+    test('handles an invalid row placement',() => {
+      let isValid = solver.checkRowPlacement(puzzleString,'A',1,'1');
+      assert.isFalse(isValid);
     });
 
-    // Returns the expected solution for a valid, incomplete puzzle
-    test('Returns the expected solution for an incomplete puzzle', done => {
-      const input = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
-      
-      // done();
+    test('handles valid column placement',() => {
+      let isValid = solver.checkColPlacement(puzzleString,'A',1,'7');
+      assert.isTrue(isValid);
+    });
+
+    test('handles an invalid column placement',() => {
+      let isValid = solver.checkColPlacement(puzzleString,'A',1,'6');
+      assert.isFalse(isValid);
+    });
+
+    test('handles a valid region (3x3 grid) placement',() => {
+      let isValid = solver.checkRegionPlacement(puzzleString,'A',1,'1');
+      assert.isTrue(isValid);
+    });
+
+    test('handles an invalid region (3x3 grid) placement', () => {
+      let isValid = solver.checkRegionPlacement(puzzleString,'A',1,'5');
+      assert.isFalse(isValid);
     });
   });
-});
+   
+  suite('Solver',() => {
+    let puzzleString = '..9..5.1.85.4....2432......1...69.83.9.....6.62.71...9......1945....4.37.4.3..6..';
+    test('valid puzzle strings pass the solver',() => {
+      assert.isTrue(solver.solve(puzzleString).result);
+    });
+
+    test('invalid puzzle strings pass the solver',() => {
+      assert.isFalse(solver.solve(puzzleString.slice(1)).result);
+    });
+
+    test('returns the expected solution for an incomplete puzzle',() => {
+      assert.isTrue(solver.solve(puzzleString).result);
+    });
+  });
+    
+     });
+
+
+
